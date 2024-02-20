fa165fb25c25984e22440fd4120e25bee1bb1b44
Pablo Garcia <garciandres.15@gmail.com>
2024-02-20 15:40:21 +0100

    Remove unused library

--- 05-testing/laboratorio/src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
+++ 05-testing/laboratorio/src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 import userEvent from '@testing-library/user-event';
 
