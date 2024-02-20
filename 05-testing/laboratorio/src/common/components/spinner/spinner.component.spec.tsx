e3db708500462974bfeb2ecb2ab6199027a1f26e
Pablo Garcia <garciandres.15@gmail.com>
2024-02-20 16:04:25 +0100

    Opcional - Añadir test al componente ./src/common/components/spinner/spinner.component.tsx

---
+++ 05-testing/laboratorio/src/common/components/spinner/spinner.component.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, renderHook, screen } from '@testing-library/react';
+import { SpinnerComponent } from './spinner.component';
+import { Config } from 'react-promise-tracker';
+import * as reactPromiseTracker from 'react-promise-tracker/lib/trackerHook';
+
+describe('common/components/spinner/SpinnerComponent', () => {
+  it('should not render passing promiseInProgress as false', () => {
+    // Arrange
+    const usePromiseTrackerStub = jest
+      .spyOn(reactPromiseTracker, 'usePromiseTracker')
+      .mockImplementation(() => {
+        return { promiseInProgress: false };
+      });
+
+    // Act
+    render(<SpinnerComponent />);
+
+    // Assert
+    expect(screen.queryByRole('presentation')).not.toBeInTheDocument();
+  });
+
+  it('should render passing promiseInProgress as true', () => {
+    // Arrange
+    const usePromiseTrackerStub = jest
+      .spyOn(reactPromiseTracker, 'usePromiseTracker')
+      .mockImplementation(() => {
+        return { promiseInProgress: true };
+      });
+
+    // Act
+    render(<SpinnerComponent />);
+
+    // Assert
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+  });
+});
